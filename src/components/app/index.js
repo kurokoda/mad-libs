dbd0419a148d53e9996b37c011d02c9e0b6d18fb
Kurokoda <indigo.developer@gmail.com>
2019-03-03 17:45:54 -0500

    Improves styling

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -8,13 +8,16 @@ const App = () => (
     <div className="madlibs__container">
       <div className="madlibs__content">
         <div className="madlibs__section madlibs__section--gray">
-          <InputField label="Question One"/>
-          <InputField label="Question One"/>
-          <InputField label="Question One"/>
-          <InputField label="Question One"/>
-          <InputField label="Question One"/>
+          <h3 className="madlibs__section-label">About Me</h3>
+          <InputField label="Question One" />
+          <InputField label="Question One" />
+          <InputField label="Question One" />
+          <InputField label="Question One" />
+          <InputField label="Question One" />
         </div>
-        <div className="madlibs__section">Right Side</div>
+        <div className="madlibs__section">
+          <h3 className="madlibs__section-label">Your Essay Test</h3>
+        </div>
       </div>
     </div>
   </div>
