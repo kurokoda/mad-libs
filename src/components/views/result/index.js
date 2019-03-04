701b6da0961de7f13a4ffbd0dd934a8884248da0
Kurokoda <indigo.developer@gmail.com>
2019-03-04 01:01:18 -0500

    Improves app styling

--- src/components/views/result/index.js
+++ src/components/views/result/index.js
@@ -7,9 +7,14 @@ const ResultView = (props) => {
   return (
     <div className="madlibs__container">
       <div className="madlibs__content">
-        <div className="madlibs__section madlibs__section--gray">
-          <h3 className="madlibs__section-label">About Me</h3>
-          <button className="madlibs__button" data-target-view="input" onClick={onButtonClick}>Edit</button>
+        <div className="madlibs__section madlibs__section--centered madlibs__section--gray">
+          <div className="madlibs__result-section">
+            <h3 className="madlibs__section-label">About Me</h3>
+            <div className="madlibs__result-text">
+              Foo fooberty foo foo monkeybutt Foo fooberty foo foo monkeybutt Foo fooberty foo foo monkeybutt Foo fooberty foo foo monkeybutt Foo fooberty foo foo monkeybutt Foo fooberty foo foo monkeybutt Foo fooberty foo foo monkeybutt
+            </div>
+            <button className="madlibs__button" data-target-view="input" onClick={onButtonClick}>Edit</button>
+          </div>
         </div>
       </div>
     </div>
